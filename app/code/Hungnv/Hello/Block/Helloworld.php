1ca1bdef9992346da010c38b08e9258b06265933
nguyenhung1007 <hung1007perfect@gmail.com>
2020-05-17 19:40:44 +0700

    edit

--- app/code/Hungnv/Hello/Block/Helloworld.php
+++ app/code/Hungnv/Hello/Block/Helloworld.php
@@ -5,6 +5,6 @@ class Helloworld extends \Magento\Framework\View\Element\Template
 {
     public function getHelloWorldTxt()
     {
-        return 'Hello world Nguyễn Hùng!';
+        return 'Xin Chào Nguyễn Hùng!';
     }
 }
